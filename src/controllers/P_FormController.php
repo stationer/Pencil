dc30c8989eca24b8fd516d5a3c7c4f28f4a9473c
Andrew Leach <andrew@leachcreative.com>
2018-08-09 16:15:57 -0400

    WEB-4: Build Scaffolding For Pencil

---
+++ src/controllers/P_FormController.php
@@ -0,0 +1,85 @@
+<?php
+/**
+ * P_FormController - Form Controller
+ *
+ * PHP version 7.0
+ *
+ * @category Graphite
+ * @package  Pencil
+ * @author   Andrew Leach <andrew@leachcreative.com>
+ * @license  CC BY-NC-SA http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * @link     http://g.lonefry.com
+ */
+namespace Stationer\Pencil\controllers;
+
+use Stationer\Graphite\View;
+use Stationer\Graphite\Controller;
+use Stationer\Graphite\data\IDataProvider;
+
+/**
+ * Class P_FormController
+ * @package Stationer\Pencil\controllers
+ * @category Pencil
+ * @author   Andrew Leach <andrew@leachcreative.com>
+ * @license  CC BY-NC-SA http://creativecommons.org/licenses/by-nc-sa/3.0/
+ * @link     http://g.lonefry.com
+ */
+class P_FormController extends Controller {
+
+    /**
+     * Controller constructor
+     *
+     * @param array         $argv Argument list passed from Dispatcher
+     * @param IDataProvider $DB   DataProvider to use with Controller
+     * @param View          $View Graphite View helper
+     */
+    public function __construct(array $argv = [], IDataProvider $DB = null, View $View = null) {
+        parent::__construct($argv, $DB, $View);
+
+        if (!G::$S->roleTest('Admin/Login')) {
+            return parent::do_403($argv);
+        }
+    }
+
+    /**
+     * List forms
+     *
+     * @param array $argv    Argument list passed from Dispatcher
+     * @param array $request Request_method-specific parameters
+     *
+     * @return View
+     */
+    public function do_list(array $argv = array(), array $request = array()) {
+
+
+        return $this->View;
+    }
+
+    /**
+     * Add a new form
+     *
+     * @param array $argv    Argument list passed from Dispatcher
+     * @param array $request Request_method-specific parameters
+     *
+     * @return View
+     */
+    public function do_add(array $argv = array(), array $request = array()) {
+
+
+        return $this->View;
+    }
+
+    /**
+     * Search form submission items
+     *
+     * @param array $argv    Argument list passed from Dispatcher
+     * @param array $request Request_method-specific parameters
+     *
+     * @return View
+     */
+    public function do_submissions(array $argv = array(), array $request = array()) {
+
+
+        return $this->View;
+    }
+}
---
+++ src/libs/ArboristWorkflow.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: andrew_leach
+ * Date: 8/9/18
+ * Time: 9:49 AM
+ */
+
+namespace Stationer\Pencil\libs;
+
+
+class ArboristWorkflow {
+    public function create() {
+
+    }
+
+    public function getByPath($path, $create) {
+
+    }
+
+    public function getChildren($parent_id) {
+
+    }
+
+    public function getById($id) {
+
+    }
+
+    public function getByURL($url) {
+
+    }
+
+    public function getByContent($id, $type) {
+
+    }
+
+    public function move($Node, $newParent) {
+
+    }
+
+    public function getContent($Node) {
+
+    }
+
+    public function delete() {
+
+    }
+
+    public function copy($newLabel) {
+
+    }
+
+}
