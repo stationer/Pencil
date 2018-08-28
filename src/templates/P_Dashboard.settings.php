a5c2d1fc018a5ac7b9c6a55413247aae6a515897
Andrew Leach <andrew@leachcreative.com>
2018-08-28 11:18:47 -0400

    WEB-6b: Dashboard Settings

--- src/templates/P_Dashboard.settings.php
+++ src/templates/P_Dashboard.settings.php
@@ -5,12 +5,36 @@ echo $View->render('header'); ?>
             <div class="col-md-12">
                 <h1>Site Settings</h1>
 
+                <form action="/P_Dashboard/settings" method="post">
+                    <?php d($Site); ?>
+                    <div class="form-group">
+                        <label for="theme">Site Theme</label>
+                        <select name="theme_id" class="form-control">
+                            <?php foreach($Themes as $Theme) : ?>
+                                <option value="<?php echo $Theme->node_id; ?>" <?php echo $Theme->node_id == $Site->theme_id ? 'selected':''; ?>><?php echo $Theme->label; ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="defaultPage_id">Default Page</label>
+                        <select name="defaultPage_id" class="form-control">
+                            <?php foreach($Pages as $Page) : ?>
+                                <option value="<?php echo $Page->node_id; ?>"><?php echo $Page->label; ?></option>
+                            <?php endforeach; ?>
+                        </select>
+                    </div>
+
+                    <button class="btn btn-primary" type="submit">Update Theme.</button>
+                </form>
+
+
                 <?php
-                \croak($SiteNode->toArray());
+/*                \croak($SiteNode->toArray());
                 \croak($Site->toArray());
                 \croak(array_keys($Themes));
                 \croak(array_keys($Pages));
-                ?>
+                */?>
             </div>
         </div>
     </div>
