83a3df3b0b9e01538c1af36552ea26077c81c97e
Adam Stafford <adam.stafford@bettercarpeople.com>
2018-10-16 14:53:27 -0400

    add chevron-down image

--- src/js/Pencil.js
+++ src/js/Pencil.js
@@ -1,7 +1,40 @@
 (function() {
+
+	//Modal
+	var modal_btn = document.querySelectorAll('[data-modal]');
+	var close_btn = document.querySelectorAll('[data-dismiss]');
+
+	for (var i = 0; i < modal_btn.length; i++) {
+		var thisBtn = modal_btn[i];
+		thisBtn.addEventListener("click", function() {
+			var modal = document.getElementById(this.dataset.modal);
+			modal.style.display = "block";
+			modal.classList.add("opened");
+
+		}, false);
+	}
+
+	for (var i = 0; i < close_btn.length; i++) {
+		var thisBtn = close_btn[i];
+		thisBtn.addEventListener("click", function() {
+			var modal = document.getElementById(this.dataset.dismiss);
+			modal.style.display = "none";
+			modal.classList.remove("opened");
+		}, false);
+	}
+
+	window.onclick = function(event) {
+		var modal_open = document.getElementsByClassName('c-modal opened')[0];
+		if (event.target == modal_open) {
+			modal_open.style.display = "none";
+			modal_open.classList.remove("opened");
+		}
+	};
+
     //Feather Icons initialize
     feather.replace();
 
+    //Navigation Dropdown
     var coll = document.querySelectorAll(".c-nav-menu > li a");
     var i;
 
